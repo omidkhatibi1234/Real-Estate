dcf5fc91e1c00a0923d511edcfc832fb35ca943b
omid khatibi <omidkhatibiofficialy@gmail.com>
2024-11-23 19:26:13 +0330

    created About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
+      <About />
     </div>
   );
 };
