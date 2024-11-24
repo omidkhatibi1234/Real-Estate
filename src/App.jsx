324c22cf76987f029f449291dbf589928c688d94
omid khatibi <omidkhatibiofficialy@gmail.com>
2024-11-24 16:19:59 +0330

    created Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Projects />
       <Testimonails />
       <Contact />
+      <Footer />
     </div>
   );
 };
