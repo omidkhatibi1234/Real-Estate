9e3c16e05852fdd33eb1612eafe96da99e88f112
omid khatibi <omidkhatibiofficialy@gmail.com>
2024-11-24 14:00:27 +0330

    created Contact section & maked functionality to user to send Email

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
@@ -6,10 +10,12 @@ import Testimonails from "./components/Testimonails";
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonails />
+      <Contact />
     </div>
   );
 };
